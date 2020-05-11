f77fb6fb32098f7d72d6d660c558526ac6655352
Karol Witkowski <karol.witkowski@hotmail.com>
2020-05-11 21:36:11 +0200

    Add app logo and button styles
    Styled app logo, fixed buttons position

--- src/components/Theme.vue
+++ src/components/Theme.vue
@@ -4,7 +4,7 @@
 
 <script>
 export default {
-  name: 'theme',
+  name: 'Theme',
 };
 </script>
 
--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="navbar">
-    <router-link to="/" style="padding-right: 1%;">
-      <button class="routerBtn">
-        <span class="routerText">HOME</span>
+    <div class="logo">
+      <img src="@/assets/chart-bell-curve.png" alt="bell curve chart icon" class="logoIcon">
+      <span class="namePart1">ESTI</span>
+      <span class="namePart2">RENCY</span>
+    </div>
+    <router-link to="/about">
+      <button class="routerAboutBtn">
+        <span class="routerText">ABOUT</span>
       </button>
     </router-link>
-    <router-link to="/about">
-      <button class="routerBtn">
-        <span class="routerText">ABOUT</span>
+    <router-link to="/">
+      <button class="routerHomeBtn">
+        <span class="routerText">HOME</span>
       </button>
     </router-link>
   </div>
