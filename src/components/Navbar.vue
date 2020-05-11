4773d6854f9f71a0c75d26b16f2fa0a5d01ad572
Karol Witkowski <karol.witkowski@hotmail.com>
2020-05-11 06:34:03 +0200

    Add route to logo
    Added route to logo, remove btn active class

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,19 @@
 <template>
     <nav>
         <v-toolbar>
-            <v-toolbar-title class="text-uppercase deep-purple--text" router :to="'/'">
+         <v-btn text route :to="'/'"
+       @click="changeBackgroundColor = !changeBackgroundColor"
+       :style="{
+           backgroundColor : changeBackgroundColor ? 'white !important' : '',
+           color: changeBackgroundColor ? 'white !important' : 'white'
+           }">
+            <v-toolbar-title class="text-uppercase deep-purple--text">
                 <v-icon left size="26" color="deep-purple lighten-2" class="mb-2 mx-1">
                   mdi-chart-bell-curve</v-icon>
                 <span class="font-weight-light">esti</span>
                 <span class="">rency</span>
             </v-toolbar-title>
+          </v-btn>
             <v-spacer />
                 <v-btn outlined class="mr-2" width="110" color="deep-purple lighten-1"
                  v-for="link in links"
@@ -34,5 +41,4 @@ export default {
 </script>
 
 <style lang="scss">
-$primary: '#fab';
 </style>
