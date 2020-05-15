e77c85213754ca923a6339c0aaeb9d5d4be19024
Karol Witkowski <karol.witkowski@hotmail.com>
2020-05-15 23:51:18 +0200

    Prototype of menu
    Simple prototype of left menu (select currency)

--- src/components/selectmenu/Select.vue
+++ src/components/selectmenu/Select.vue
@@ -2,10 +2,14 @@
     <div class="select">
       <p class="menu">MENU</p>
       <p class="base">SELECT BASE CURRENCY</p>
+      <v-select :options="options" placeholder="select base currency"
+       class="style-chooser"></v-select>
     </div>
 </template>
 
 <script>
+
+
 export default {
   name: 'Select',
 };
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
+import vSelect from 'vue-select';
+import 'vue-select/src/scss/vue-select.scss';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+Vue.component('v-select', vSelect);
 Vue.config.productionTip = false;
 
 new Vue({
