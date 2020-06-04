b4678ff3e6c90075f2361282c4971942b29b7d14
Karol Witkowski <karol.witkowski@hotmail.com>
2020-06-04 04:31:23 +0200

    Setup axios
    Make axios get rest api for default value from store

--- src/components/rate/Rate.vue
+++ src/components/rate/Rate.vue
@@ -1,15 +1,38 @@
 <template>
   <div class="rate">
-    <p>{{ $store.state.baseCurrency.name }}</p>
-    <p>{{ $store.state.wantedCurrency.name }}</p>
-    <p>{{ $store.state.amountValue }}</p>
-  </div>
+    <div>
+      <span>{{ $store.state.amountValue }}</span> {{ $store.state.baseCurrency.cc }}
+      ({{ $store.state.baseCurrency.name }}) is
+      </div>
+    <div>
+      XXX {{ $store.state.wantedCurrency.cc }}
+    </div>
+    <div>
+     The rate of transaction is {{ss}}
+    </div>
+    </div>
 </template>
 
 <script>
+import axios from 'axios';
+
+const currencyRate = 'https://api.exchangeratesapi.io/latest?base=';
+
 export default {
   name: 'Rate',
+  data() {
+    return {
+      response: [],
+      getBaseCurrency: this.$store.state.baseCurrency.cc,
+    };
+  },
   methods: {
+  },
+  mounted() {
+    axios.get(`${currencyRate}${this.getBaseCurrency}`)
+      .then((response) => {
+        console.log(response);
+      });
   },
 };
 </script>
