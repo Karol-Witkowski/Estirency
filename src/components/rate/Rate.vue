b8831c8120ee898fb15ff328bbe528440ea2e6e8
Karol Witkowski <karol.witkowski@hotmail.com>
2020-06-09 21:52:15 +0200

    Target specific value from API respons, clean up configs

--- src/components/rate/Rate.vue
+++ src/components/rate/Rate.vue
@@ -40,7 +40,7 @@ export default {
   mounted() {
     axios.get(`${currencyRate}${this.setBaseCurrency}`)
       .then((response) => {
-        this.rate = response.data.rates;
+        this.rate = response.data.rates[this.setWantedCurrency];
       })
       .catch((error) => {
         console.log(error);
@@ -49,7 +49,7 @@ export default {
   updated() {
     axios.get(`${currencyRate}${this.setBaseCurrency}`)
       .then((response) => {
-        this.rate = response.data.rates;
+        this.rate = response.data.rates[this.setWantedCurrency];
       })
       .catch((error) => {
         console.log(error);
