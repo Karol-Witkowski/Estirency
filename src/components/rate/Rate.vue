e0cd237c7ce5e988bc76e9553f8a9b2de5dff77d
Karol Witkowski <karol.witkowski@hotmail.com>
2020-06-10 04:14:50 +0200

    Introduce calculated values
    Calculate amount value, round results

--- src/components/rate/Rate.vue
+++ src/components/rate/Rate.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="rate">
     <div>
+     The rate of transaction is {{ setRate }}
+    </div>
+    <div>
       <span>
         {{ $store.state.amountValue }}
       </span>
          {{ $store.state.baseCurrency.cc }} ({{ $store.state.baseCurrency.name }}) is
     </div>
     <div>
-      XXX {{ $store.state.wantedCurrency.cc }}
-    </div>
-    <div>
-     The rate of transaction is {{ rate }}
+      {{ setAmount }} {{ $store.state.wantedCurrency.cc }} ({{ $store.state.wantedCurrency.name }})
     </div>
     </div>
 </template>
@@ -33,6 +33,12 @@ export default {
     },
     setWantedCurrency() {
       return this.$store.state.wantedCurrency.cc;
+    },
+    setRate() {
+      return Math.round(this.rate * 1000) / 1000;
+    },
+    setAmount() {
+      return Math.round((this.$store.state.amountValue * this.setRate) * 100) / 100;
     },
   },
   methods: {
