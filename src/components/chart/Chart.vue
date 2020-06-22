beacd58efd1366b8d8b0cd21725528a04c257555
Karol Witkowski <karol.witkowski@hotmail.com>
2020-06-23 00:06:31 +0200

    Set chart component
    Set basic styles, import amcharts

--- src/components/chart/Chart.vue
+++ src/components/chart/Chart.vue
@@ -1,8 +1,18 @@
 <template>
-    <div class="chart"></div>
+    <div class="chart">
+      <p class="chartDescription">historical exchange rates from <span class="year">1990</span></p>
+    </div>
 </template>
 
 <script>
+import * as am4core from '@amcharts/amcharts4/core';
+// eslint-disable-next-line no-unused-vars
+import * as am4charts from '@amcharts/amcharts4/charts';
+// eslint-disable-next-line camelcase
+import am4themes_animated from '@amcharts/amcharts4/themes/animated';
+
+am4core.useTheme(am4themes_animated);
+
 export default {
   name: 'Rate',
 };
