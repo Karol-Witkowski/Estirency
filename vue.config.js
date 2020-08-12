35a1f88609d1991de8d5655ebf06554e1e3d0256
Karol Witkowski <karol.witkowski@hotmail.com>
2020-08-12 19:32:02 +0200

    Vue.config change

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,3 @@
 module.exports = {
-  transpileDependencies: [
-  ],
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/https://karol-witkowski.github.io'
-    : '/Estirency',
+  publicPath: '/Estirency/',
 };
