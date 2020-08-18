78a98bf0def1207dac5a326d1de5d49d684eee2c
Karol Witkowski <karol.witkowski@hotmail.com>
2020-08-18 05:30:55 +0200

    Fix script error
    Fix deploy script

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -4,18 +4,16 @@ const execa = require("execa");
   (async () => {
     try {
       await execa("git", ["checkout", "--orphan", "gh-pages"]);
-      console.log("Building...");
+      console.log("Building in progress...");
       await execa("npm", ["run", "build"]);
-      // Understand if it's dist or build folder
       const folderName = fs.existsSync("dist") ? "dist" : "build";
       await execa("git", ["--work-tree", folderName, "add", "--all"]);
       await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
       console.log("Pushing to gh-pages...");
       await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-      await execa("rm", ["-r", folderName]);
       await execa("git", ["checkout", "-f", "master"]);
       await execa("git", ["branch", "-D", "gh-pages"]);
-      console.log("Successfully deployed");
+      console.log("Successfully deployed to gh-pages");
     } catch (e) {
       console.log(e.message);
       process.exit(1);
