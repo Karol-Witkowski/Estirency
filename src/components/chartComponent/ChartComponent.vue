883ca1463dc556ab0ec4118ea6a121ec0d9131fb
Karol Witkowski <karol.witkowski@hotmail.com>
2020-08-31 00:36:17 +0200

    Edit chart, add test data
    Add dummy data, change chart options

--- src/components/chartComponent/ChartComponent.vue
+++ src/components/chartComponent/ChartComponent.vue
@@ -15,7 +15,7 @@ import datachart from '@/components/chartComponent/chart/Chart.vue';
 let actualDate;
 let pastDate;
 
-const historicalRate = 'https://fcsapi.com/api-v2/forex/history?symbol= BLOCKED';
+const historicalRate = 'https://fcsapi.com/api-v2/forex/history?symbol=';
 const year = new Date().getFullYear();
 const month = new Date().getMonth() + 1;
 const day = new Date().getDate() - 1;
@@ -48,16 +48,20 @@ export default {
       historyDate: String,
       historyRate: String,
       chartdata: {
-        labels: ['Dummy1', 'Dummy2', 'Dummy3'],
+        labels: ['Dummy 1', 'Dummy 2', 'Dummy 3', 'Dummy 4', 'Dummy 5', 'Dummy 6', 'Dummy 7', 'Dummy 8', 'Dummy 9', 'Dummy 10', 'Dummy 11', 'Dummy 12', 'Dummy 13'],
         datasets: [{
-          label: 'Rates',
-          backgroundColor: '#c7b6b6',
-          data: [1000, 500, 200],
+          borderColor: '#c7b6b6',
+          pointBackgroundColor: '#a68b8b',
+          pointHoverBorderWidth: 3,
+          data: [12, 15, 10, 9, 10, 9, 12, 11, 10, 8, 9, 7, 9],
         }],
       },
       options: {
         responsive: true,
         maintainAspectRatio: false,
+        legend: {
+          display: false,
+        },
       },
     };
   },
@@ -92,8 +96,8 @@ export default {
 
     dataLoop() {
       for (let i = 0; i < this.history.length; i += 1) {
-        this.options.xaxis.categories.push(this.history[i].tm);
-        this.series[0].data.push(this.history[i].c);
+        this.chartdata.labels.push(this.history[i].tm);
+        this.chartdata.datasets.data.push(this.history[i].c);
       }
     },
   },
