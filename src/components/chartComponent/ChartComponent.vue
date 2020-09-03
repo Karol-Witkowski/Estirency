822c8a85e34d9083daa96cad808b118fb8c78342
Karol Witkowski <karol.witkowski@hotmail.com>
2020-09-03 03:49:10 +0200

    Force chart data refresh
    Reset data and labels on chart update

--- src/components/chartComponent/ChartComponent.vue
+++ src/components/chartComponent/ChartComponent.vue
@@ -16,7 +16,7 @@ import datachart from '@/components/chartComponent/chart/Chart.vue';
 let actualDate;
 let pastDate;
 
-const historicalRate = 'https://fcsapi.com/api-v2/forex/history?symbol= d';
+const historicalRate = 'https://fcsapi.com/api-v2/forex/history?symbol=DELETETHIS';
 const year = new Date().getFullYear();
 const month = new Date().getMonth() + 1;
 const day = new Date().getDate() - 1;
@@ -105,6 +105,8 @@ export default {
   },
 
   updated() {
+    this.chartData.labels = [];
+    this.chartData.datasets[0].data = [];
     this.getData();
   },
 };
