4f729213de782a190ab08f44387fc54ccb84c286
Karol Witkowski <karol.witkowski@hotmail.com>
2020-12-03 23:15:55 +0100

    Fix - undefined jest
    Add global env to fix problem

--- vue.config.js
+++ vue.config.js
@@ -10,9 +10,9 @@ module.exports = {
               loader: 'sass-resources-loader',
               options: {
                 resources: [
+                  'src/assets/styles/mixins/components.scss',
                   'src/assets/styles/vars/colors.scss',
                   'src/assets/styles/vars/typography.scss',
-                  'src/assets/styles/mixins/components.scss',
                 ],
               },
             },
