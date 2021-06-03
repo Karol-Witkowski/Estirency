4a702408d7713a682af41156a18b85a1c98bc9b8
Karol Witkowski <karol.witkowski@hotmail.com>
2021-06-03 05:21:51 +0200

    test(e2e): query elements by name

--- tests/e2e/menu.spec.js
+++ tests/e2e/menu.spec.js
@@ -7,97 +7,75 @@ describe('Menu test', () => {
     cy.get('[data-cy=rate]')
       .find('span')
       .contains('100');
-    cy.get('.amountInput')
+    cy.get('input[name=amount]')
       .type('a252..123');
     cy.get('[data-cy=rate]')
-      .find('span')
+      .get('span[name=baseAmount]')
       .contains('252.12');
   });
 
   it('Check that the selected currencies are displayed in the description and chart panels', () => {
-    cy.get('section>span')
-      .eq(1)
+    cy.get('span[name=base]')
       .contains('zł (Polish zloty)');
-    cy.get('section>span')
-      .eq(3)
+    cy.get('span[name=target]')
       .contains('€ (European Euro)');
-    cy.get('h3')
-      .eq(2)
+    cy.get('h3[name=currencyHeader]')
       .contains('PLN/EUR');
-    cy.get('input')
-      .eq(0)
+    cy.get('input[name=baseCurrency]')
       .type('croatian')
       .click();
     cy.get('section')
       .contains('Croatian kuna')
       .click();
-    cy.get('input')
-      .eq(1)
+    cy.get('input[name=targetCurrency]')
       .type('british')
       .click();
     cy.get('section')
       .contains('British pound')
-      .click({ force: true });
-    cy.get('section>span')
-      .eq(1)
+      .click();
+    cy.get('span[name=base]')
       .contains('kn (Croatian kuna)');
-    cy.get('section>span')
-      .eq(3)
+    cy.get('span[name=target]')
       .contains('£ (British pound)');
-    cy.get('h3')
-      .eq(2)
+    cy.get('h3[name=currencyHeader]')
       .contains('HRK/GBP');
   });
 
   it('Check that the typed currencies are displayed in the description and chart panels', () => {
-    cy.get('section>span')
-      .eq(1)
+    cy.get('span[name=base]')
       .contains('zł (Polish zloty)');
-    cy.get('section>span')
-      .eq(3)
+    cy.get('span[name=target]')
       .contains('€ (European Euro)');
-    cy.get('h3')
-      .eq(2)
+    cy.get('h3[name=currencyHeader]')
       .contains('PLN/EUR');
-    cy.get('input')
-      .eq(0)
+    cy.get('input[name=baseCurrency]')
       .click()
       .type('croatian{enter}');
-    cy.get('input')
-      .eq(1)
+    cy.get('input[name=targetCurrency]')
       .click()
       .type('british{enter}');
-    cy.get('section>span')
-      .eq(1)
+    cy.get('span[name=base]')
       .contains('kn (Croatian kuna)');
-    cy.get('section>span')
-      .eq(3)
+    cy.get('span[name=target]')
       .contains('£ (British pound)');
-    cy.get('h3')
-      .eq(2)
+    cy.get('h3[name=currencyHeader]')
       .contains('HRK/GBP');
   });
 
   it('Check swapping currencies', () => {
-    cy.get('section>span')
-      .eq(1)
+    cy.get('span[name=base]')
       .contains('zł (Polish zloty)');
-    cy.get('section>span')
-      .eq(3)
+    cy.get('span[name=target')
       .contains('€ (European Euro)');
-    cy.get('h3')
-      .eq(2)
+    cy.get('h3[name=currencyHeader]')
       .contains('PLN/EUR');
     cy.get('[data-cy=swapIcon]')
       .click();
-    cy.get('section>span')
-      .eq(1)
+    cy.get('span[name=base]')
       .contains('€ (European Euro)');
-    cy.get('section>span')
-      .eq(3)
+    cy.get('span[name=target]')
       .contains('zł (Polish zloty)');
-    cy.get('h3')
-      .eq(2)
+    cy.get('h3[name=currencyHeader]')
       .contains('EUR/PLN');
   });
 });
